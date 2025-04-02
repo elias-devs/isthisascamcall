0b3d439b1a8acd45143be84715fc31e2018f7e3c
Elias Shanaa <eshan_dev@pm.me>
2025-04-01 23:49:59 -0700

    Added vite integration

--- isthisascamcall-ts/src/main.ts
+++
@@ -1,9 +0,0 @@
-// main.ts
-async function getNumberData(number: string) {
-    const res = await fetch("/data/fakenums.json");
-    const data = await res.json();
-    console.log("Fetched data from browser:", data);  // <--- make sure this is here
-    return data;
-}
-
-getNumberData("8331234567");
